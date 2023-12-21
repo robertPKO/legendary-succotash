8f449928621c5060327e01d53cd34769d11cdff1
Robert Tran <roberttran@ad.pakko.com.au>
2023-12-21 16:16:56 +1000

    first commit

---
+++ src/components/canvas/Examples.jsx
@@ -0,0 +1,77 @@
+'use client'
+
+import { Html, useGLTF } from '@react-three/drei'
+import { useFrame } from '@react-three/fiber'
+import * as THREE from 'three'
+import { useMemo, useRef, useState } from 'react'
+import { Line, useCursor, MeshDistortMaterial } from '@react-three/drei'
+import { useRouter } from 'next/navigation'
+
+export const Blob = ({ route = '/', ...props }) => {
+  const router = useRouter()
+  const [hovered, hover] = useState(false)
+  useCursor(hovered)
+  return (
+    <mesh
+      onClick={() => router.push(route)}
+      onPointerOver={() => hover(true)}
+      onPointerOut={() => hover(false)}
+      {...props}>
+      <sphereGeometry args={[1, 64, 64]} />
+      <MeshDistortMaterial roughness={0} color={hovered ? 'hotpink' : '#1fb2f5'} />
+    </mesh>
+  )
+}
+
+export const Logo = ({ route = '/blob', ...props }) => {
+  const mesh = useRef(null)
+  const router = useRouter()
+
+  const [hovered, hover] = useState(false)
+  const points = useMemo(() => new THREE.EllipseCurve(0, 0, 3, 1.15, 0, 2 * Math.PI, false, 0).getPoints(100), [])
+
+  useCursor(hovered)
+  useFrame((state, delta) => {
+    const t = state.clock.getElapsedTime()
+    mesh.current.rotation.y = Math.sin(t) * (Math.PI / 8)
+    mesh.current.rotation.x = Math.cos(t) * (Math.PI / 8)
+    mesh.current.rotation.z -= delta / 4
+  })
+
+  return (
+    <group ref={mesh} {...props}>
+      {/* @ts-ignore */}
+      <Line worldUnits points={points} color='#1fb2f5' lineWidth={0.15} />
+      {/* @ts-ignore */}
+      <Line worldUnits points={points} color='#1fb2f5' lineWidth={0.15} rotation={[0, 0, 1]} />
+      {/* @ts-ignore */}
+      <Line worldUnits points={points} color='#1fb2f5' lineWidth={0.15} rotation={[0, 0, -1]} />
+      <mesh onClick={() => router.push(route)} onPointerOver={() => hover(true)} onPointerOut={() => hover(false)}>
+        <sphereGeometry args={[0.55, 64, 64]} />
+        <meshPhysicalMaterial roughness={0} color={hovered ? 'hotpink' : '#1fb2f5'} />
+                  <Html position={[0, 0, 0]} transform={true}>
+            <div className="bg-white w-[5000px] h-[5000px] rounded-[16%] cursor-pointer group overflow-hidden relative border-collapse">
+              {" "}
+              <div className="bg-red w-full h-full -z-1 absolute transform left-0 top-[100%] rounded-[16%] transition-all duration-2000 ease-in group-hover:top-0 group-hover:opacity-100 opacity-0 " />
+              <p className=" absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-[1000px] z-1 group-hover:text-white font-bold transition-all duration-500 ease-out">
+                Play
+              </p>
+            </div>
+          </Html>
+      </mesh>
+    </group>
+  )
+}
+
+export function Duck(props) {
+  const { scene } = useGLTF('/duck.glb')
+
+  useFrame((state, delta) => (scene.rotation.y += delta))
+
+  return <primitive object={scene} {...props} />
+}
+export function Dog(props) {
+  const { scene } = useGLTF('/dog.glb')
+
+  return <primitive object={scene} {...props} />
+}
---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  mode: 'jit',
+  content: ['./app/**/*.{js,ts,jsx,tsx}', './src/**/*.{js,ts,jsx,tsx}'], // remove unused styles in production
+  darkMode: 'media', // or 'media' or 'class'
+  theme: {
+    extend: {
+      backgroundImage: {
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic':
+          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      },
+      colors:{
+        red:"#FF0000"
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
